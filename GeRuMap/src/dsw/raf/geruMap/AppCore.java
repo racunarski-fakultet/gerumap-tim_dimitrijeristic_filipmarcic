12cc8c4429b1ba8f5c5143ede408a35b40ad2d58
filipmarcic <fmarcic14822rn@raf.rs>
2022-11-06 14:31:46 +0100

    dodat lombok

--- GeRuMap/src/dsw/raf/geruMap/AppCore.java
+++ GeRuMap/src/dsw/raf/geruMap/AppCore.java
@@ -1,8 +1,22 @@
 package dsw.raf.geruMap;
+
+import java.lang.reflect.AccessibleObject;
+
 public class AppCore
 {
+    private AppCore instance = null;
     public static void main(String Args[])
     {
 
+    }
+    public AppCore getInstance()
+    {
+        if(instance.equals(null))
+            instance = new AppCore();
+
+        return instance;
+    }
+    private AppCore()
+    {
     }
 }
