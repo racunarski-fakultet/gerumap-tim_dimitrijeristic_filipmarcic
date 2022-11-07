79ece0ce1fb87c96c388eb03e9f1233eab8a1132
filipmarcic <fmarcic14822rn@raf.rs>
2022-11-07 09:24:20 +0100

    Info meni promene 1

--- GeRuMap/src/dsw/raf/geruMap/gui/swing/view/InfoFrame.java
+++ GeRuMap/src/dsw/raf/geruMap/gui/swing/view/InfoFrame.java
@@ -30,37 +30,36 @@ public class InfoFrame extends JFrame {
     }
     private void initialiseGUI(Icon img)
     {
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension scr_size = kit.getScreenSize();
-        int scr_height = scr_size.height;
-        int scr_width = scr_size.width;
-        setSize(scr_width/3, scr_height/3);
+        setSize(300,200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle("Info");
 
         JLabel name = new JLabel("Ime: ");
+        name.setBorder(BorderFactory.createEmptyBorder(7,5,7,5));
         JLabel surname = new JLabel("Prezime: ");
+        surname.setBorder(BorderFactory.createEmptyBorder(7,5,7,5));
         JLabel index = new JLabel("Indeks: ");
+        index.setBorder(BorderFactory.createEmptyBorder(7,5,7,5));
 
         JPanel text = new JPanel();
-        text.setMinimumSize(new Dimension(200,200));
+        text.setBorder(BorderFactory.createEmptyBorder(7,5,7,5));
         text.add(name);
         text.add(surname);
         text.add(index);
-        text.setLayout(new BoxLayout(text,BoxLayout.Y_AXIS));
+        text.setLayout(new BoxLayout(text,BoxLayout.PAGE_AXIS));
 
         ImageIcon test = new ImageIcon(this.img.getImage());
         ImagePanel imeg = new ImagePanel(test.getImage());
-        imeg.setMinimumSize(new Dimension(200,200));
+        imeg.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 
         JPanel all = new JPanel();
         all.setLayout(new GridLayout());
-        all.setMinimumSize(new Dimension(400,400));
         all.add(imeg);
         all.add(text);
 
-
+        this.setMinimumSize(new Dimension(300,200));
+        this.setMaximumSize(new Dimension(350,200));
         this.add(all);
     }
 }
