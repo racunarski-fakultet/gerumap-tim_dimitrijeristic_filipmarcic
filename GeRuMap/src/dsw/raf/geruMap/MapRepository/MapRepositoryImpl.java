7a994db48bbd06efd472f31d02b12c6742bc4462
dristic <dristic4321rn@raf.rs>
2022-11-07 13:13:43 +0100

    Dodat map reporsitory u appcore

--- GeRuMap/src/dsw/raf/geruMap/MapRepository/MapRepositoryImpl.java
+++ GeRuMap/src/dsw/raf/geruMap/MapRepository/MapRepositoryImpl.java
@@ -1,8 +1,23 @@
 package dsw.raf.geruMap.MapRepository;
 
+import dsw.raf.geruMap.MapRepository.Composite.MapNode;
+import dsw.raf.geruMap.MapRepository.Composite.MapNodeComposite;
+import dsw.raf.geruMap.MapRepository.Implementation.ProjectExplorer;
 import dsw.raf.geruMap.core.MapRepository;
 
 public class MapRepositoryImpl implements MapRepository
 {
+    private ProjectExplorer projectExplorer;
+
+    public MapRepositoryImpl() {
+        projectExplorer = new ProjectExplorer();
+    }
+
+    @Override
+    public ProjectExplorer getProjectExplorer() {
+        return projectExplorer;
+    }
+
+
 
 }
--- GeRuMap/src/dsw/raf/geruMap/core/MapRepository.java
+++ GeRuMap/src/dsw/raf/geruMap/core/MapRepository.java
@@ -1,5 +1,11 @@
 package dsw.raf.geruMap.core;
 
+import dsw.raf.geruMap.MapRepository.Composite.MapNode;
+import dsw.raf.geruMap.MapRepository.Composite.MapNodeComposite;
+import dsw.raf.geruMap.MapRepository.Implementation.ProjectExplorer;
+
 public interface MapRepository
 {
+    ProjectExplorer getProjectExplorer();
+
 }
