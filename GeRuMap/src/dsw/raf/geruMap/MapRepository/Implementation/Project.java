b42c462d8263f37fa284e63bf7d820d94228fae6
filipmarcic <fmarcic14822rn@raf.rs>
2022-11-13 21:01:26 +0100

    m

--- GeRuMap/src/dsw/raf/geruMap/MapRepository/Implementation/Project.java
+++ GeRuMap/src/dsw/raf/geruMap/MapRepository/Implementation/Project.java
@@ -1,7 +1,9 @@
 package dsw.raf.geruMap.MapRepository.Implementation;
 
+import dsw.raf.geruMap.AppCore;
 import dsw.raf.geruMap.MapRepository.Composite.MapNode;
 import dsw.raf.geruMap.MapRepository.Composite.MapNodeComposite;
+import dsw.raf.geruMap.MapRepository.MapRepositoryImpl;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,4 +22,18 @@ public class Project extends MapNodeComposite
     }
     @Override
     protected boolean allowsChild(MapNode child) {return child instanceof MindMap;}
+
+    @Override
+    public boolean add_child(MapNode child) {
+        super.add_child(child);
+        ((MapRepositoryImpl)AppCore.getInstance().getRep()).publish(this);
+        return true;
+    }
+
+    @Override
+    public boolean delete_child(MapNode child) {
+        super.delete_child(child);
+        ((MapRepositoryImpl)AppCore.getInstance().getRep()).publish(this);
+        return true;
+    }
 }
