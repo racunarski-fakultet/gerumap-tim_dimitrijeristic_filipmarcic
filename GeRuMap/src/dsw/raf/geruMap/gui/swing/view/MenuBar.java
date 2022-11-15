db9639beb426932e8ab70bf2ddee1fe36c23c866
dristic <dristic4321rn@raf.rs>
2022-11-15 20:04:39 +0100

    Dodata autor akcija u meni

--- GeRuMap/src/dsw/raf/geruMap/gui/swing/view/MenuBar.java
+++ GeRuMap/src/dsw/raf/geruMap/gui/swing/view/MenuBar.java
@@ -14,11 +14,13 @@ public class MenuBar extends JMenuBar
         JMenuItem info_a = new JMenuItem(MainFrame.getInstance().getAct_man().getInf_act());
         JMenuItem rename_a = new JMenuItem(MainFrame.getInstance().getAct_man().getRename_act());
         JMenuItem delete_a = new JMenuItem(MainFrame.getInstance().getAct_man().getDelete_act());
+        JMenuItem autor_a = new JMenuItem(MainFrame.getInstance().getAct_man().getAutor_act());
 
         file_menu.add(exit_a);
         file_menu.add(new_a);
         file_menu.add(rename_a);
         file_menu.add(delete_a);
+        file_menu.add(autor_a);
         help_menu.add(info_a);
 
         this.add(file_menu);
