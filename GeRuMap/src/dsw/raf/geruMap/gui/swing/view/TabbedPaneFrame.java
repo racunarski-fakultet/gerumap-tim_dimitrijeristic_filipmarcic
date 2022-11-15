d825b92bb2dc5f62f2e5e90afe6d2c0ded03ad95
filipmarcic <fmarcic14822rn@raf.rs>
2022-11-15 16:25:17 +0100

    Poboljsan observer v2

--- GeRuMap/src/dsw/raf/geruMap/gui/swing/view/TabbedPaneFrame.java
+++ GeRuMap/src/dsw/raf/geruMap/gui/swing/view/TabbedPaneFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class TabbedPaneFrame extends JPanel {
     public TabbedPaneFrame() {
         if(this.getParent()!=null)
-        this.setSize(this.getParent().getSize());
+            this.setSize(this.getParent().getSize());
         this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
     }
 }
