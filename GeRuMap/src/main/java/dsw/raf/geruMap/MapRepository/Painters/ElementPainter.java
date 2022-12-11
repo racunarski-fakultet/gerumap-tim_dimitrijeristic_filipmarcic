751b1e67d890725a70ccf8952606187aeb700bae
filip <fmarcic14822rn@raf.rs>
2022-12-11 21:01:54 +0100

    Nedelja 08 gotova (sa malim problemima)

--- GeRuMap/src/main/java/dsw/raf/geruMap/MapRepository/Painters/ElementPainter.java
+++ GeRuMap/src/main/java/dsw/raf/geruMap/MapRepository/Painters/ElementPainter.java
@@ -18,16 +18,16 @@ public abstract class ElementPainter {
     {
         this.element = element;
 
-        shape=new GeneralPath();
-        ((GeneralPath)shape).moveTo(element.getPosition().x,element.getPosition().y);
-
-        ((GeneralPath)shape).lineTo(element.getPosition().x+element.getSize().width,element.getPosition().y);
-
-        ((GeneralPath)shape).lineTo(element.getPosition().x+element.getSize().width,element.getPosition().y+element.getSize().height);
-
-        ((GeneralPath)shape).lineTo(element.getPosition().x,element.getPosition().y+element.getSize().height);
-
-        ((GeneralPath)shape).closePath();
+//        shape=new GeneralPath();
+//        ((GeneralPath)shape).moveTo(element.getPosition().x,element.getPosition().y);
+//
+//        ((GeneralPath)shape).lineTo(element.getPosition().x+element.getSize().width,element.getPosition().y);
+//
+//        ((GeneralPath)shape).lineTo(element.getPosition().x+element.getSize().width,element.getPosition().y+element.getSize().height);
+//
+//        ((GeneralPath)shape).lineTo(element.getPosition().x,element.getPosition().y+element.getSize().height);
+//
+//        ((GeneralPath)shape).closePath();
     }
 
     public abstract void paint(Graphics2D g);
--- GeRuMap/src/main/java/dsw/raf/geruMap/StateManager/States/DeleteState.java
+++ GeRuMap/src/main/java/dsw/raf/geruMap/StateManager/States/DeleteState.java
@@ -1,7 +1,10 @@
 package dsw.raf.geruMap.StateManager.States;
 
 import dsw.raf.geruMap.MapRepository.Implementation.Element;
+import dsw.raf.geruMap.MapRepository.Implementation.Link;
 import dsw.raf.geruMap.MapRepository.Painters.ElementPainter;
+import dsw.raf.geruMap.MapRepository.Painters.LinkPainter;
+import dsw.raf.geruMap.MapRepository.Painters.ThoughtPainter;
 import dsw.raf.geruMap.gui.swing.tree.model.MapTreeItem;
 import dsw.raf.geruMap.gui.swing.view.MainFrame;
 import dsw.raf.geruMap.gui.swing.view.MapView;
@@ -19,15 +22,32 @@ public class DeleteState implements AbstractState {
     @Override
     public void mousePress(int x, int y, MapTreeItem node)
     {
+        ElementPainter temp = null;
+
         for (ElementPainter i : ((MapView)MainFrame.getInstance().getDesktop().getSelectedComponent()).getElems())
         {
             if (i.elementAt(i.getElement(),new Point(x,y)))
             {
                 System.out.println("deleted");
-                MainFrame.getInstance().getMapTree().findNode(i.getElement()).delete();
+                temp = i;
             }
-
         }
+
+        if (temp instanceof ThoughtPainter)
+        {
+            for (ElementPainter i : ((MapView)MainFrame.getInstance().getDesktop().getSelectedComponent()).getElems())
+            {
+                if (i instanceof LinkPainter)
+                {
+                    if (((Link)i.getElement()).getParentThought().equals(temp.getElement()) || ((Link)i.getElement()).getChildThought().equals(temp.getElement()))
+                    {
+                        MainFrame.getInstance().getMapTree().findNode(i.getElement()).delete();
+                    }
+                }
+            }
+        }
+
+        MainFrame.getInstance().getMapTree().findNode(temp.getElement()).delete();
     }
 
     @Override
